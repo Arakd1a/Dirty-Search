f2880dabf0b4b86972f1cb47c32b0ed64eb18fb0
Matt Barker <matt@barker.space>
2016-12-03 22:56:20 +1000

    Create DirtySearch.java

---
+++ DirtySearch.java
@@ -0,0 +1,18 @@
+
+package dirtysearch;
+
+import java.awt.Color;
+
+public class DirtySearch {
+Window window;
+   
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.setVisible(true);
+           window.getContentPane().setBackground(Color.WHITE);  //Whatever color
+        
+        
+        
+    }
+    
+}
